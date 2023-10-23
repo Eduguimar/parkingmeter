05e474386820b75e208fe0bd6480cab7922960a5
Eduardo <eduguimara@gmail.com>
2023-10-23 19:40:49 -0300

    Initial commit

---
+++ src/test/java/com/fiap/postech/parkingmeter/ParkingmeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.postech.parkingmeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingmeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
