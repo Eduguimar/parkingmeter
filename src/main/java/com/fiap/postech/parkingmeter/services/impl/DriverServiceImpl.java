9f9311eabe55efd0d85fd5eeec12aab813e7bdf2
Eduardo <eduguimara@gmail.com>
2023-10-25 21:12:49 -0300

    Services implementation created

--- src/main/java/com/fiap/postech/parkingmeter/services/impl/DriverServiceImpl.java
+++ src/main/java/com/fiap/postech/parkingmeter/services/impl/DriverServiceImpl.java
@@ -1,43 +1,83 @@
 package com.fiap.postech.parkingmeter.services.impl;
 
 import com.fiap.postech.parkingmeter.dtos.DriverDTO;
+import com.fiap.postech.parkingmeter.dtos.VehicleDTO;
 import com.fiap.postech.parkingmeter.models.Driver;
+import com.fiap.postech.parkingmeter.models.Vehicle;
 import com.fiap.postech.parkingmeter.repositories.DriverRepository;
+import com.fiap.postech.parkingmeter.repositories.VehicleRepository;
 import com.fiap.postech.parkingmeter.services.DriverService;
+import com.fiap.postech.parkingmeter.services.exceptions.ControllerNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class DriverServiceImpl implements DriverService {
 
     @Autowired
     DriverRepository driverRepository;
+    @Autowired
+    VehicleRepository vehicleRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public Page<DriverDTO> findAll(PageRequest pageRequest) {
         Page<Driver> drivers = driverRepository.findAll(pageRequest);
         return drivers.map(driver -> new DriverDTO(driver, driver.getVehicles()));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public DriverDTO findById(Long id) {
-        return null;
+        var driver = driverRepository.findById(id).orElseThrow(() -> new ControllerNotFoundException("Driver not found"));
+        return new DriverDTO(driver, driver.getVehicles());
     }
 
     @Override
+    @Transactional
     public DriverDTO save(DriverDTO driverDTO) {
-        return null;
+        Driver driver = new Driver();
+        mapperDtoToEntity(driverDTO, driver);
+        Driver savedDriver = driverRepository.save(driver);
+        return new DriverDTO(savedDriver, savedDriver.getVehicles());
     }
 
     @Override
+    @Transactional
     public DriverDTO update(Long id, DriverDTO driverDTO) {
-        return null;
+        try {
+            Driver driver = driverRepository.getOne(id);
+            mapperDtoToEntity(driverDTO, driver);
+            return new DriverDTO(driver, driver.getVehicles());
+        } catch (NoSuchElementException e) {
+            throw new ControllerNotFoundException("Driver not found, id: " + id);
+        }
     }
 
     @Override
     public void delete(Long id) {
+        try {
+            driverRepository.deleteById(id);
+        } catch (NoSuchElementException e) {
+            throw new ControllerNotFoundException("Driver not found, id: " + id);
+        }
+    }
 
+    private void mapperDtoToEntity(DriverDTO dto, Driver driver) {
+        driver.setName(dto.getName());
+        driver.setDocument(dto.getDocument());
+        driver.setBirthDate(dto.getBirthDate());
+        driver.setMail(dto.getMail());
+        driver.setPhone(dto.getPhone());
+
+        for (VehicleDTO vehicleDTO : dto.getVehicles()) {
+            Vehicle vehicle = vehicleRepository.getOne(vehicleDTO.getId());
+            driver.getVehicles().add(vehicle);
+        }
     }
 }
--- src/main/java/com/fiap/postech/parkingmeter/services/impl/VehicleServiceImpl.java
+++ src/main/java/com/fiap/postech/parkingmeter/services/impl/VehicleServiceImpl.java
@@ -1,35 +1,75 @@
 package com.fiap.postech.parkingmeter.services.impl;
 
 import com.fiap.postech.parkingmeter.dtos.VehicleDTO;
+import com.fiap.postech.parkingmeter.models.Vehicle;
+import com.fiap.postech.parkingmeter.repositories.DriverRepository;
+import com.fiap.postech.parkingmeter.repositories.VehicleRepository;
 import com.fiap.postech.parkingmeter.services.VehicleService;
+import com.fiap.postech.parkingmeter.services.exceptions.ControllerNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @Service
 public class VehicleServiceImpl implements VehicleService {
+
+    @Autowired
+    VehicleRepository vehicleRepository;
+    @Autowired
+    DriverRepository driverRepository;
+
     @Override
+    @Transactional(readOnly = true)
     public Page<VehicleDTO> findAll(PageRequest pageRequest) {
-        return null;
+        Page<Vehicle> vehicles = vehicleRepository.findAll(pageRequest);
+        return vehicles.map(VehicleDTO::new);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public VehicleDTO findById(Long id) {
-        return null;
+        var vehicle = vehicleRepository.findById(id).orElseThrow(() -> new ControllerNotFoundException("Vehicle not found"));
+        return new VehicleDTO(vehicle);
     }
 
     @Override
-    public VehicleDTO save(VehicleDTO driverDTO) {
-        return null;
+    @Transactional
+    public VehicleDTO save(VehicleDTO vehicleDTO) {
+        Vehicle vehicle = new Vehicle();
+        mapperDtoToEntity(vehicleDTO, vehicle);
+        Vehicle savedVehicle = vehicleRepository.save(vehicle);
+        return new VehicleDTO(savedVehicle);
     }
 
     @Override
-    public VehicleDTO update(Long id, VehicleDTO driverDTO) {
-        return null;
+    @Transactional
+    public VehicleDTO update(Long id, VehicleDTO vehicleDTO) {
+        try {
+            Vehicle vehicle = vehicleRepository.getOne(id);
+            mapperDtoToEntity(vehicleDTO, vehicle);
+            return new VehicleDTO(vehicleRepository.save(vehicle));
+        } catch (NoSuchElementException e) {
+            throw new ControllerNotFoundException("Vehicle not found, id: " + id);
+        }
     }
 
     @Override
     public void delete(Long id) {
+        try {
+            vehicleRepository.deleteById(id);
+        } catch (NoSuchElementException e) {
+            throw new ControllerNotFoundException("Vehicle not found, id: " + id);
+        }
+    }
 
+    private void mapperDtoToEntity(VehicleDTO dto, Vehicle vehicle) {
+        vehicle.setModel(dto.getModel());
+        vehicle.setLicensePlate(dto.getLicensePlate());
+        vehicle.setYear(dto.getYear());
+        vehicle.setDriver(driverRepository.getOne(dto.getDriver().getId()));
     }
 }
