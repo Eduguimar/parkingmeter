536accfae2f74932ea87b49a4f8c3f58963f398f
Eduardo <eduguimara@gmail.com>
2023-10-26 21:30:27 -0300

    Driver and Vehicle controllers created

--- src/main/java/com/fiap/postech/parkingmeter/controllers/DriverController.java
+++ src/main/java/com/fiap/postech/parkingmeter/controllers/DriverController.java
@@ -1,9 +1,57 @@
 package com.fiap.postech.parkingmeter.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fiap.postech.parkingmeter.dtos.DriverDTO;
+import com.fiap.postech.parkingmeter.services.DriverService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/drivers")
 public class DriverController {
 
+    @Autowired
+    private DriverService driverService;
 
+    @GetMapping
+    public ResponseEntity<Page<DriverDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<DriverDTO> drivers = driverService.findAll(pageRequest);
+
+        return ResponseEntity.ok(drivers);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<DriverDTO> findById(@PathVariable Long id) {
+        var driver = driverService.findById(id);
+
+        return ResponseEntity.ok(driver);
+    }
+
+    @PostMapping
+    public ResponseEntity<DriverDTO> save(@RequestBody DriverDTO driverDTO) {
+        var driver = driverService.save(driverDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(driver);
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<DriverDTO> update(@PathVariable Long id, @RequestBody DriverDTO driverDTO) {
+        var driver = driverService.update(id, driverDTO);
+
+        return ResponseEntity.ok(driver);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        driverService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Driver deleted successfully!");
+    }
 }
--- src/main/java/com/fiap/postech/parkingmeter/controllers/VehicleController.java
+++ src/main/java/com/fiap/postech/parkingmeter/controllers/VehicleController.java
@@ -1,7 +1,57 @@
 package com.fiap.postech.parkingmeter.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.fiap.postech.parkingmeter.dtos.VehicleDTO;
+import com.fiap.postech.parkingmeter.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(value = "/vehicles")
 public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @GetMapping
+    public ResponseEntity<Page<VehicleDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, size);
+        Page<VehicleDTO> vehicles = vehicleService.findAll(pageRequest);
+
+        return ResponseEntity.ok(vehicles);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<VehicleDTO> findById(@PathVariable Long id) {
+        var vehicle = vehicleService.findById(id);
+
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @PostMapping
+    public ResponseEntity<VehicleDTO> save(@RequestBody VehicleDTO vehicleDTO) {
+        var vehicle = vehicleService.save(vehicleDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(vehicle);
+    }
+
+    @PostMapping(value = "/{id}")
+    public ResponseEntity<VehicleDTO> update(@PathVariable Long id, @RequestBody VehicleDTO vehicleDTO) {
+        var vehicle = vehicleService.update(id, vehicleDTO);
+
+        return ResponseEntity.ok(vehicle);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id) {
+        vehicleService.delete(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Vehicle deleted successfully!");
+    }
 }
